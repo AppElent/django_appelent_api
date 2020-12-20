d1a3a4dde5179e1ae4a686f51c6c6fca7b407579
Eric Jansen <ericjansen@live.nl>
2020-12-20 17:01:51 +0100

    create meterstanden day and month view

--- api/views/MeterstandViewSet.py
+++ api/views/MeterstandViewSet.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from ..serializers import MeterstandSerializer
 from ..models import Meterstand
 from ..permissions import IsOwner
@@ -27,10 +28,29 @@ class MeterstandViewSet(viewsets.ModelViewSet):
         many = True if isinstance(request.data, list) else False
         serializer = MeterstandSerializer(data=request.data, many=many)
         if serializer.is_valid():
-            for meterstand in request.data:
-                Meterstand.objects.update_or_create(user=request.user, datetime=meterstand['datetime'], defaults=meterstand)
+            if many:
+                for meterstand in request.data:
+                    Meterstand.objects.update_or_create(user=request.user, datetime=meterstand['datetime'], defaults=meterstand)
+            else:
+                #request.data.pop('csrfmiddlewaretoken', None)
+                data = {x: request.data[x] for x in request.data if x not in ['csrfmiddlewaretoken']}
+                Meterstand.objects.update_or_create(user=request.user, datetime=request.data['datetime'], defaults=data)
             #serializer.save()
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=['get'], url_path='day/(?P<day>\d{4}-\d{2}-\d{2})/$')#'day/(?P<day>\w+)')
+    def day(self, request, day, *args, **kwargs):
+        datearray = day.split('-')
+        meterstanden = Meterstand.objects.filter(user=self.request.user, datetime__year=datearray[0], datetime__month=datearray[1], datetime__day=datearray[2])
+        meterstanden_data = self.get_serializer_class()(meterstanden, many=True)
+        return Response(meterstanden_data.data)
+
+    @action(detail=False, methods=['get'], url_path='month/(?P<month>\d{4}-\d{2})/$')
+    def month(self, request, month, *args, **kwargs):
+        datearray = month.split('-')
+        meterstanden = Meterstand.objects.filter(user=self.request.user, datetime__year=datearray[0], datetime__month=datearray[1])
+        meterstanden_data = self.get_serializer_class()(meterstanden, many=True)
+        return Response(meterstanden_data.data)
 
