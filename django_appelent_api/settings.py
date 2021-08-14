8ae825fecf0fa4048b5dd23e002d041ba227c51e
Eric Jansen <ericjansen@live.nl>
2021-08-14 19:17:14 +0200

    fix autofield

--- django_appelent_api/settings.py
+++ django_appelent_api/settings.py
@@ -61,6 +61,11 @@ ALLOWED_HOSTS = [
     'localhost:3000'
 ]
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Application definition
 
 INSTALLED_APPS = [
